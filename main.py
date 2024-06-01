60704d4b1c8c0d7e5762e3a3e10ef32765d60c7d
copipate <160614059+copipate@users.noreply.github.com>
2024-06-02 00:25:36 +0700

    kagle

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import json
 from check import creates_categories, creation_json_yolo_to_coco
 
-annotation_directory = r"/kaggle/input/data-training-caries/Data_Training_caries/train/images"
-images_directory = r"/kaggle/input/data-training-caries/Data_Training_caries/train/labels"
+annotation_directory = r"/kaggle/input/data-training-caries/Data_Training_caries/train/labels"
+images_directory = r"/kaggle/input/data-training-caries/Data_Training_caries/train/images"
 
 
 
